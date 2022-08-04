61bf91b19785040f538ab82422655c0b289cfb8d
enk <elifnurber@hotmail.com>
2022-08-04 10:12:56 +0300

    two way binding and pipes added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
+
+
 
 @NgModule({
   declarations: [
@@ -9,7 +13,8 @@ import { ProductListComponent } from './products/product-list.component';
     ProductListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -9,6 +9,8 @@ export class ProductListComponent{
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin: number = 2;
+    showImage: boolean = false;
+    listFilter: string = 'cart';
     products: any[] = [
         {
             "productId": 2,
@@ -31,4 +33,7 @@ export class ProductListComponent{
             "imageUrl": "assets/images/hammer.png"
         }
     ];
+    toggleImage(): void{
+        this.showImage = ! this.showImage;
+    }
 }
