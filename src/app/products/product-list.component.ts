38324df50ba4ce7bde1d505b85dc23ba3574d993
enk <elifnurber@hotmail.com>
2022-08-09 14:12:07 +0300

    retrieving data using HTTP(observable, rxjs)

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from "@angular/core";
-import { filter } from "rxjs";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { filter, Subscriber, Subscription } from "rxjs";
 import { IProduct } from "./product";
 import { ProductService } from "./product.service";
 
@@ -9,11 +9,13 @@ import { ProductService } from "./product.service";
     styleUrls: ['./product-list.component.css']
 })
 
-export class ProductListComponent implements OnInit {
+export class ProductListComponent implements OnInit, OnDestroy {
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
+    errorMessage: string = '';
+    sub!: Subscription;
 
     private _listFilter: string = '';
     get listFilter(): string {
@@ -28,20 +30,32 @@ export class ProductListComponent implements OnInit {
     filteredProducts: IProduct[] = [];
     products: IProduct[] = [];
 
-    constructor(private productService: ProductService){}
+    constructor(private productService: ProductService) { }
 
     toggleImage(): void {
         this.showImage = !this.showImage;
     }
+
     ngOnInit(): void {
-        this.products = this.productService.getProducts();
-        this.filteredProducts = this.products;
+        this.sub = this.productService.getProducts().subscribe({
+            next: products => {
+                this.products = products;
+                this.filteredProducts = this.products;
+            },
+            error: err => this.errorMessage = err
+        });
     }
+
+    ngOnDestroy(): void {
+        this.sub.unsubscribe();
+    }
+
     performFilter(filterby: string): IProduct[] {
         filterby = filterby.toLocaleLowerCase();
         return this.products.filter((product: IProduct) =>
-        product.productName.toLocaleLowerCase().includes(filterby));
+            product.productName.toLocaleLowerCase().includes(filterby));
     }
+
     onRatingClicked(message: string): void {
         this.pageTitle = "Product List: " + message;
     }
